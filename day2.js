5fbeacd08818b01b9f3bfe6ea71912f3f28e90f4
maheshbasnet089 <basnetmanish089@gmail.com>
2025-03-06 21:27:59 +0545

    first commit

---
+++ day2.js
@@ -0,0 +1,34 @@
+
+
+// console.log(1+106) // print 
+// console.log("bye world")
+
+// node ko version 18.10 --> watch mode --> automatically rerun or restart
+
+// variable 
+
+// var/let/const variableName = value 
+// primitive datatype --> string , number, boolean
+// non primitive datatype --> array, object
+
+let cup = "chiyaa" // string
+let gamala = "sagh"  // string
+
+let bag = "book"
+let test = "1bssswwss"
+
+const pi = 3.14  // number
+const gravity = 9.8 // number
+
+let isNepali = true // boolean
+let isIndian = false
+
+const password = "thisissecretpasswords"
+
+console.log("Mero gamala ma chai yo cha? k xa ? " + gamala + " xa")
+console.log(`Mero gamala ma chai yo cha? k xa ? ${gamala} xa `)
+
+let age = 22 
+console.log("My age is " + age)
+console.log(`My age is ${age}`)
+
